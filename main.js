3102c6445c154e5e4cb99c96ed405bde6c6bc4dc
cytrees <88301996+cytrees@users.noreply.github.com>
2021-08-08 10:36:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function sign()
+{
+    window.alert("You Have Already Created A Account Redirect To Your Account?")
+    window.location = "account.html";
+}
+
+function loan()
+{
+    window.location = "loan.html";
+}
+
+function fifty()
+{
+    window.alert("Your Bank Says : You Took A Loan From CYTREES LOAN SERVICE About 50Rs To Collect : Go To Your Neartest V.I.P Service Centre");
+    localStorage.setItem("50Rs");
+}
+
+function hundered()
+{
+    window.alert("Your Bank Says : You Took A Loan From CYTREES LOAN SERVICE About 100Rs To Collect : Go To Your Neartest V.I.P Service Centre");
+    localStorage.setItem("100Rs");
+}
+
+function onefifty()
+{
+    window.alert("Your Bank Says : You Took A Loan From CYTREES LOAN SERVICE About 150Rs To Collect : Go To Your Neartest V.I.P Service Centre");
+    localStorage.setItem("150Rs");
+}
